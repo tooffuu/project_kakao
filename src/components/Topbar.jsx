e6afdceb2cc649f2e8175813f8a8ce5951bef15f
ttoffuu <thdud8866@naver.com>
2022-08-12 18:48:12 +0900

    메뉴바

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -8,40 +8,126 @@ const Topbar = () => {
         <title>카카오뱅크</title>
       </head>
 
-      <body>
-        <nav className="menu-bar">
-          <div className="menu-bar-1">
-            <ul className="logo-1">
-              <li className="kakaoimage">
-                <a href="#">
-                  <img
-                    src="https://www.kakaobank.com/static/images/web/logo_black.svg"
-                    alt=""
-                  />
-                </a>
-              </li>
-              <li className="nav-item">
+      <nav className="menu-bar">
+        <div className="menu-bar-1">
+          <ul className="logo-1">
+            <li className="kakaoimage">
+              <a href="#">
+                <img
+                  src="https://www.kakaobank.com/static/images/web/logo_black.svg"
+                  alt=""
+                />
+              </a>
+            </li>
+          </ul>
+          <div className="items">
+            <ul className="nav-item">
+              <li>
                 <a href="#">은행소개</a>
+                <div>
+                  <div>
+                    <ul>
+                      <li>
+                        <a href="#">2차 아이템</a>
+                        <ul>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#">2차 아이템</a>
+                        <ul>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#">2차 아이템</a>
+                        <ul>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <a href="#">2차 아이템</a>
+                        <ul>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                          <li>
+                            <a href="#">3차 아이템</a>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </li>
-              <li className="nav-item">
+            </ul>
+            <ul className="nav-item">
+              <li>
                 <a href="#">IR투자정보</a>
               </li>
-              <li className="nav-item">
+            </ul>
+            <ul className="nav-item">
+              <li>
                 <a href="#">상품안내</a>
               </li>
-              <li className="nav-item">
+            </ul>
+            <ul className="nav-item">
+              <li>
                 <a href="#">고객센터</a>
               </li>
-              <li className="nav-item">
+            </ul>
+            <ul className="nav-item">
+              <li>
                 <a href="#">이벤트</a>
               </li>
-              <li className="nav-item submit">
+            </ul>
+            <ul className="nav-item submit">
+              <li>
                 <a href="#">서류제출하기</a>
               </li>
             </ul>
           </div>
-        </nav>
-      </body>
+        </div>
+      </nav>
     </>
   );
 };
