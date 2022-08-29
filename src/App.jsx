69aac47b13a53a70be51f87e6f3a3e767c5c58c4
tooffuu <thdud8866@naver.com>
2022-08-29 18:37:53 +0900

    react_Router 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Topbar from "./components/Topbar";
 import Content1 from "./components/Content1";
 import Content2 from "./components/Content2";
@@ -10,21 +11,31 @@ import Content7 from "./components/Content7";
 import Content8 from "./components/Content8";
 import Content9 from "./components/Content9";
 import Foot from "./components/Foot";
+import Rode from "./components/Rode";
 
 const App = () => {
   return (
     <>
-      <Topbar />
-      <Content1 />
-      <Content2 />
-      <Content3 />
-      <Content4 />
-      <Content5 />
-      <Content6 />
-      <Content7 />
-      <Content8 />
-      <Content9 />
-      <Foot />
+      <BrowserRouter>
+        <Switch>
+          <Route path={"/"} exact>
+            <Topbar />
+            <Content1 />
+            <Content2 />
+            <Content3 />
+            <Content4 />
+            <Content5 />
+            <Content6 />
+            <Content7 />
+            <Content8 />
+            <Content9 />
+            <Foot />
+          </Route>
+          <Route path={"/rodemap"} exact>
+            <Rode />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/components/Rode.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rode = () => {
+  return (
+    <div>
+      <h2>안녕하세요.</h2>
+    </div>
+  );
+};
+
+export default Rode;
